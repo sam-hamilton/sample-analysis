e122c98dc392ef24c78e751610d9139260daf489
Sam Hamilton <sam@4ds.co.uk>
2022-05-26 15:58:54 +0100

    Redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::middleware(['auth'])->group(function () {
