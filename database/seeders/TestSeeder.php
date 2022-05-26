0141df686a6d981fe71db6b00ae7da0d1905913b
Sam Hamilton <sam@4ds.co.uk>
2022-05-26 14:53:09 +0100

    Typo fix

--- database/seeders/TestSeeder.php
+++ database/seeders/TestSeeder.php
@@ -17,7 +17,7 @@ class TestSeeder extends Seeder
     {
         DB::table('tests')->insert([
             ['type' => 'talvitis', 'analysis_service' => 'https://bwsho71wt6.execute-api.eu-west-2.amazonaws.com/api/tests/analyse/', 'analysis_service_token' => 'xqvzfFppKgFZ3LU8iKbCngOpBdRW2D2d', 'created_at' => now(), 'updated_at' => now()],
-            ['type' => 'samabeties', 'analysis_service' => 'https://keaho22vr7.execute-api.eu-west-2.example.com/api/tests/analyse/', 'analysis_service_token' => Str::random(32), 'created_at' => now(), 'updated_at' => now()],
+            ['type' => 'samabetes', 'analysis_service' => 'https://keaho22vr7.execute-api.eu-west-2.example.com/api/tests/analyse/', 'analysis_service_token' => Str::random(32), 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
