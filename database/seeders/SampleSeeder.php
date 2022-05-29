ab1e35b3313b13b4a3f93adf87a9219d4758e497
Sam Hamilton <sam@4ds.co.uk>
2022-05-30 00:27:52 +0100

    Sample seeder now uses test image

--- database/seeders/SampleSeeder.php
+++ database/seeders/SampleSeeder.php
@@ -20,9 +20,9 @@ class SampleSeeder extends Seeder
 
         foreach ($tests as $test) {
             DB::table('samples')->insert([
-                ['user_id' => rand($users->min(), $users->max()), 'test_id' => $test->id, 'test_strip' => $test->type . '/' . Str::random() . '.jpg', 'result' => 'Positive', 'analysis_failed' => false, 'reading_one_name' => 'control', 'reading_one_value' => 0, 'reading_two_name' => 'detect', 'reading_two_value' => 0, 'created_at' => now(), 'updated_at' => now()],
-                ['user_id' => rand($users->min(), $users->max()), 'test_id' => $test->id, 'test_strip' => $test->type . '/' . Str::random() . '.jpg', 'result' => 'Failed To Detect Cartridge', 'analysis_failed' => true, 'reading_one_name' => 'control', 'reading_one_value' => 0, 'reading_two_name' => 'detect', 'reading_two_value' => 0, 'created_at' => now(), 'updated_at' => now()],
-                ['user_id' => rand($users->min(), $users->max()), 'test_id' => $test->id, 'test_strip' => $test->type . '/' . Str::random() . '.jpg', 'result' => 'Invalid Test (no control line detected)', 'analysis_failed' => false, 'reading_one_name' => 'control', 'reading_one_value' => 1, 'reading_two_name' => 'detect', 'reading_two_value' => 0.96, 'created_at' => now(), 'updated_at' => now()],
+                ['user_id' => rand($users->min(), $users->max()), 'test_id' => $test->id, 'test_strip' => 'test/test.png', 'result' => 'Positive', 'analysis_failed' => false, 'reading_one_name' => 'control', 'reading_one_value' => 0, 'reading_two_name' => 'detect', 'reading_two_value' => 0, 'created_at' => now(), 'updated_at' => now()],
+                ['user_id' => rand($users->min(), $users->max()), 'test_id' => $test->id, 'test_strip' => 'test/test.png', 'result' => 'Failed To Detect Cartridge', 'analysis_failed' => true, 'reading_one_name' => 'control', 'reading_one_value' => 0, 'reading_two_name' => 'detect', 'reading_two_value' => 0, 'created_at' => now(), 'updated_at' => now()],
+                ['user_id' => rand($users->min(), $users->max()), 'test_id' => $test->id, 'test_strip' => 'test/test.png', 'result' => 'Invalid Test (no control line detected)', 'analysis_failed' => false, 'reading_one_name' => 'control', 'reading_one_value' => 1, 'reading_two_name' => 'detect', 'reading_two_value' => 0.96, 'created_at' => now(), 'updated_at' => now()],
             ]);
         }
     }
