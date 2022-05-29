2aaf876f256e986a20bbaa3bf8531503dc2b3cad
Sam Hamilton <sam@4ds.co.uk>
2022-05-29 22:52:04 +0100

    Adding integration test

---
+++ tests/Feature/AwsAnalysisServiceTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Analysis\AwsAnalysisService;
+use App\Models\Sample;
+use App\Models\Test;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/**
+ * @group integration
+ */
+class AwsAnalysisServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $test;
+    protected $sample;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->test = Test::create([
+            'type' => 'talvitis',
+            'analysis_service' => 'https://bwsho71wt6.execute-api.eu-west-2.amazonaws.com/api/tests/analyse/',
+            'analysis_service_token' => 'xqvzfFppKgFZ3LU8iKbCngOpBdRW2D2d',
+        ]);
+
+        $this->sample = Sample::create([
+            'user_id' => 999,
+            'test_id' => $this->test->id,
+            'test_strip' => '/test/test.png',
+        ]);
+    }
+
+    /** @test **/
+    public function can_get_valid_response_from_the_api()
+    {
+        $response = (new AwsAnalysisService($this->sample))->analyse()->response;
+
+        $this->assertArrayHasKey('data', $response);
+        $this->assertArrayHasKey('result', $response['data']);
+        $this->assertIsArray($response['data']);
+        $this->assertArrayHasKey('failed', $response['data']);
+        $this->assertArrayHasKey('readings', $response['data']);
+        $this->assertArrayHasKey('readings', $response['data']);
+        $this->assertIsArray($response['data']['readings']);
+        foreach ($response['data']['readings'] as $reading) {
+            $this->assertArrayHasKey('name', $reading);
+            $this->assertArrayHasKey('value', $reading);
+        }
+
+        $this->assertIsString($response['data']['result']);
+        $this->assertIsBool($response['data']['failed']);
+        foreach ($response['data']['readings'] as $reading) {
+            $this->assertIsString($reading['name']);
+            $this->assertIsNumeric($reading['value']);
+        }
+    }
+
+    /** @test **/
+    public function can_populate_sample_from_api_response()
+    {
+        $response = (new AwsAnalysisService($this->sample))->analyse()->save();
+
+        $this->assertIsString($this->sample->result);
+        $this->assertIsBool($this->sample->analysis_failed);
+        $this->assertIsString($this->sample->reading_one_name);
+        $this->assertIsNumeric($this->sample->reading_one_value);
+        $this->assertIsString($this->sample->reading_two_name);
+        $this->assertIsNumeric($this->sample->reading_two_value);
+    }
+}
